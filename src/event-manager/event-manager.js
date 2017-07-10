927daa936f7ff8ef54ae542d80f3880d12abc5de
vsesh <v.shmyroff@ya.ru>
2017-07-10 21:55:39 +0300

    step1

--- src/event-manager/event-manager.js
+++ src/event-manager/event-manager.js
@@ -1,6 +1,16 @@
 ym.modules.define('shri2017.imageViewer.EventManager', [
 ], function (provide) {
 
+    var EVENTS = {
+        mousedown: 'start',
+        mousemove: 'move',
+        mouseup: 'end',
+        touchstart: 'start',
+        touchmove: 'move',
+        touchend: 'end',
+        touchcancel: 'end'
+    };
+
     function EventManager(elem, callback) {
         this._elem = elem;
         this._callback = callback;
@@ -13,11 +23,16 @@ ym.modules.define('shri2017.imageViewer.EventManager', [
         },
 
         _setupListeners: function () {
-
+            this._mouseListener = this._mouseEventHandler.bind(this);
+            this._touchListener = this._touchEventHandler.bind(this);
+            this._addEventListeners('mousedown', this._elem, this._mouseListener);
+            this._addEventListeners('touchstart touchmove touchend touchcancel', this._elem, this._touchListener);
         },
 
         _teardownListeners: function () {
-
+            this._removeEventListeners('mousedown', this._elem, this._mouseListener);
+            this._removeEventListeners('mousemove mouseup', document.documentElement, this._mouseListener);
+            this._removeEventListeners('touchstart touchmove touchend touchcancel', this._elem, this._touchListener);
         },
 
         _addEventListeners: function (types, elem, callback) {
@@ -30,6 +45,49 @@ ym.modules.define('shri2017.imageViewer.EventManager', [
             types.split(' ').forEach(function (type) {
                 elem.removeEventListener(type, callback);
             }, this);
+        },
+
+        _mouseEventHandler: function (event) {
+            event.preventDefault();
+
+            if (event.type === 'mousedown') {
+                this._addEventListeners('mousemove mouseup', document.documentElement, this._mouseListener);
+            } else if (event.type === 'mouseup') {
+                this._removeEventListeners('mousemove mouseup', document.documentElement, this._mouseListener);
+            }
+
+            var elemOffset = this._calculateElementPreset(this._elem);
+
+            this._callback({
+                type: EVENTS[event.type],
+                targetPoint: {
+                    x: event.pageX - elemOffset.x,
+                    y: event.pageY - elemOffset.y
+                }
+            });
+        },
+
+        _touchEventHandler: function (event) {
+            // Отменяем стандартное поведение (последующие события мышки)
+            event.preventDefault();
+
+            var touches = event.touches;
+            // touchend/touchcancel
+            if (touches.length === 0) {
+                touches = event.changedTouches;
+            }
+
+            var elemOffset = this._calculateElementPreset(this._elem);
+
+            var targetPoint = {
+                x: touches[0].pageX - elemOffset.x,
+                y: touches[0].pageY - elemOffset.y
+            };
+
+            this._callback({
+                type: EVENTS[event.type],
+                targetPoint: targetPoint
+            });
         },
 
         _calculateElementPreset: function (elem) {
--- src/gesture-controller/gesture-controller.js
+++ src/gesture-controller/gesture-controller.js
@@ -8,6 +8,7 @@ ym.modules.define('shri2017.imageViewer.GestureController', [
             this._view.getElement(),
             this._eventHandler.bind(this)
         );
+        this._lastEventTypes = '';
     };
 
     Object.assign(Controller.prototype, {
@@ -17,7 +18,35 @@ ym.modules.define('shri2017.imageViewer.GestureController', [
 
         _eventHandler: function (event) {
             var state = this._view.getState();
-            // to do something awesome!
+
+            // dblclick
+            if (!this._lastEventTypes) {
+                setTimeout(function () {
+                    this._lastEventTypes = '';
+                }.bind(this), 500);
+            }
+            this._lastEventTypes += ' ' + event.type;
+
+            if (this._lastEventTypes.indexOf('start stop start stop') > -1) {
+                this._lastEventTypes = '';
+                // to do somethin awesome!
+                return;
+            }
+
+            // drag
+            if (event.type === 'move') {
+                this._processDrag(event);
+            } else {
+                this._initState = this._view.getState();
+                this._initEvent = event;
+            }
+        },
+
+        _processDrag: function (event) {
+            this._view.setState({
+                positionX: this._initState.positionX + (event.targetPoint.x - this._initEvent.targetPoint.x),
+                positionY: this._initState.positionY + (event.targetPoint.y - this._initEvent.targetPoint.y)
+            });
         }
     });
 
